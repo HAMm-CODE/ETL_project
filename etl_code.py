89929d66f192af527e8ce50584b5a71243e1e69b
Hamm-code <sentsdrimma123@gmail.com>
2026-05-12 11:34:33 +0300

    transform function

--- etl_code.py
+++ etl_code.py
@@ -25,3 +25,33 @@ def extract_from_xml(file_to_process):
         dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name, "height":height, "weight":weight}])], ignore_index=True) 
     return dataframe 
 
+def extract(): 
+    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data 
+     
+    # process all csv files, except the target file
+    for csvfile in glob.glob("*.csv"): 
+        if csvfile != target_file:  # check if the file is not the target file
+            extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True) 
+         
+    # process all json files 
+    for jsonfile in glob.glob("*.json"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True) 
+     
+    # process all xml files 
+    for xmlfile in glob.glob("*.xml"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
+         
+    return extracted_data 
+
+def transform(data): 
+    '''Convert inches to meters and round off to two decimals 
+    1 inch is 0.0254 meters '''
+    data['height'] = round(data.height * 0.0254,2) 
+ 
+    '''Convert pounds to kilograms and round off to two decimals 
+    1 pound is 0.45359237 kilograms '''
+    data['weight'] = round(data.weight * 0.45359237,2) 
+	
+    return data 
+
+

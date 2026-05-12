71e17294ff0bf6f4a981f20713a79763e308858c
Hamm-code <sentsdrimma123@gmail.com>
2026-05-12 11:32:59 +0300

    extract function

--- etl_code.py
+++ etl_code.py
@@ -3,4 +3,11 @@ import pandas as pd
 import xml.etree.ElementTree as ET 
 from datetime import datetime 
 
+log_file = "log_file.txt" 
+target_file = "transformed_data.csv" 
+
+def extract_from_csv(file_to_process): 
+	dataframe = pd.read_csv(file_to_process) 
+	return dataframe 
+
 
